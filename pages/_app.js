b9a3a088970c9a7b623ee793d193cd59b9141472
BillyJeffs <billy@memoriesofold.co.uk>
2025-10-30 17:42:27 +0000

    Configure Tailwind + fix missing deps; admin UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 
 // pages/_app.js — minimal file to load the site theme globally
+import "../styles/globals.css";
 import "../styles/paa-theme.css";
 
 export default function MyApp({ Component, pageProps }) {
